a555bc968dc75643982fc4c138195d31e0e14ef0
Alfonso Fernando Álvarez <ponchoalv@gmail.com>
2016-11-27 20:44:16 -0300

    actualización de paquetes en archvio init.el

---
+++ customizations/setup-rust.el
@@ -0,0 +1,17 @@
+
+;; Rust Mode activation
+(require 'rust-mode)
+
+;; Path to the sources of rust
+(setq racer-rust-src-path "c:/rust/src")
+
+;; Racer for rust-mode
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+
+(add-hook 'racer-mode-hook #'company-mode)
+
+;; autocomplete
+(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+(setq company-tooltip-align-annotations t)
+
